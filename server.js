09457b8f55d740f8a8a0e4b22f0cb3bec52d7b23
Dulanmalinda <dulanmalinda@gmail.com>
2024-07-14 23:30:39 +0530

    changed to port 5000 and ssl backed

--- server.js
+++ server.js
@@ -32,34 +32,36 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-// Load SSL certificates
-const privateKey = fs.readFileSync('/etc/letsencrypt/live/prompt.armorwallet.ai/privkey.pem', 'utf8');
-const certificate = fs.readFileSync('/etc/letsencrypt/live/prompt.armorwallet.ai/cert.pem', 'utf8');
-const ca = fs.readFileSync('/etc/letsencrypt/live/prompt.armorwallet.ai/chain.pem', 'utf8');
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
-const credentials = {
-  key: privateKey,
-  cert: certificate,
-  ca: ca
-};
+// // Load SSL certificates
+// const privateKey = fs.readFileSync('/etc/letsencrypt/live/prompt.armorwallet.ai/privkey.pem', 'utf8');
+// const certificate = fs.readFileSync('/etc/letsencrypt/live/prompt.armorwallet.ai/cert.pem', 'utf8');
+// const ca = fs.readFileSync('/etc/letsencrypt/live/prompt.armorwallet.ai/chain.pem', 'utf8');
 
-// Redirect HTTP to HTTPS
-const httpApp = express();
-httpApp.use((req, res, next) => {
-  if (!req.secure) {
-    return res.redirect(`https://${req.headers.host}${req.url}`);
-  }
-  next();
-});
+// const credentials = {
+//   key: privateKey,
+//   cert: certificate,
+//   ca: ca
+// };
 
-// Listen on port 5001 for HTTP requests
-const httpServer = http.createServer(httpApp);
-httpServer.listen(5001, () => {
-  console.log('HTTP Server running on port 5001');
-});
+// // Redirect HTTP to HTTPS
+// const httpApp = express();
+// httpApp.use((req, res, next) => {
+//   if (!req.secure) {
+//     return res.redirect(`https://${req.headers.host}${req.url}`);
+//   }
+//   next();
+// });
 
-// Listen on port 443 for HTTPS requests
-const httpsServer = https.createServer(credentials, app);
-httpsServer.listen(PORT, () => {
-  console.log(`HTTPS Server running on port ${PORT}`);
-});
+// // Listen on port 80 for HTTP requests
+// const httpServer = http.createServer(httpApp);
+// httpServer.listen(80, () => {
+//   console.log('HTTP Server running on port 80');
+// });
+
+// // Listen on port 443 for HTTPS requests
+// const httpsServer = https.createServer(credentials, app);
+// httpsServer.listen(PORT, () => {
+//   console.log(`HTTPS Server running on port ${PORT}`);
+// });
